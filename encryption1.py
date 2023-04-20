7d101349338037a7bbf5a9172d2dc1e1025cf00d
Ephrem Alamerew <58732304+Ephicho@users.noreply.github.com>
2023-04-20 22:14:03 +0300

    uploading encryption python code
    this code works to encrypt words using Caesar method

---
+++ encryption1.py
@@ -0,0 +1,32 @@
+# message=input('Please enetr message')
+def encrypt(key, message):
+    message = message.upper()
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ""
+
+    for letter in message:
+        if letter in alpha: #if the letter is actually a letter
+            #find the corresponding ciphertext letter in the alphabet
+            letter_index = (alpha.find(letter) + key) % len(alpha)
+
+            result = result + alpha[letter_index]
+        else:
+            result = result + letter
+
+    return result
+
+def decrypt(key, message):
+    message = message.upper()
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ""
+
+    for letter in message:
+        if letter in alpha: #if the letter is actually a letter
+            #find the corresponding ciphertext letter in the alphabet
+            letter_index = (alpha.find(letter) - key) % len(alpha)
+
+            result = result + alpha[letter_index]
+        else:
+            result = result + letter
+
+    return result
